da2bf7628ac3bf76069b27bf48227a5bfdfbae24
yangcxPusto <619980817@qq.com>
2022-08-03 03:25:05 +0100

    add artcl 0710 & dayFrmLove & PostCard & Link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
-import Avatar from '@mui/material/Avatar';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
 import Chip from '@mui/material/Chip';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import PostCard from "./components/PostCard";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { Avatar, Button, CardActionArea, CardActions } from '@mui/material';
 
 export default function App() {
   const eStart = new Date('1997-03-06T06:06:06+08:00')
@@ -17,6 +17,10 @@ export default function App() {
   const [loveDura, setLoveDura] = useState(new Date(Date.now() - loveStart));
   const [eDura, setEDura] = useState(new Date(Date.now() - eStart));
   const [fDura, setFDura] = useState(new Date(Date.now() - fStart));
+
+  function dayNumberFromLove(date) {
+    return Math.floor(new Date(new Date(date) - loveStart) / (1000*60*60*24));
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -33,9 +37,10 @@ export default function App() {
     <div>
       <Container maxWidth="sm">
       <Stack spacing={2}>
-        <Typography  variant="h1" gutterBottom>
+        <Typography  variant="h1">
           Our Love
         </Typography>
+
         <Stack direction="row" spacing={1}>
           <Chip
             avatar={<Avatar alt="avatar e" src="https://ethanyangcx.github.io/love/pic/avatarE.jpg" />}
@@ -46,51 +51,57 @@ export default function App() {
             label={Math.floor(fDura / (1000*60*60*24))}
           />
         </Stack>
+
         <Stack direction="row" spacing={1}>
-        <Chip
-            avatar={<Avatar alt="avatar heart" src="https://ethanyangcx.github.io/love/icons8-love.gif" />}
-            label={Math.floor(loveDura/ (1000*60*60*24)) + ' days and ' 
-                  + loveDura.getHours().toString().padStart(2, '0') + ':' 
-                  + loveDura.getMinutes().toString().padStart(2, '0') + ':'
-                  + loveDura.getSeconds().toString().padStart(2, '0')}
-        />
+          <Chip
+              avatar={<Avatar alt="avatar heart" src="https://ethanyangcx.github.io/love/icons8-love.gif" />}
+              label={Math.floor(loveDura/ (1000*60*60*24)) + ' days and ' 
+                    + loveDura.getHours().toString().padStart(2, '0') + ':' 
+                    + loveDura.getMinutes().toString().padStart(2, '0') + ':'
+                    + loveDura.getSeconds().toString().padStart(2, '0')}
+          />
         </Stack>
 
-          <Card sx={{}}> 
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image="https://ethanyangcx.github.io/love/pic/2022-0605-1-1.jpg"
-                alt="sweet hug in front of Westminster Abbey"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  第一次旅遊
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  倫敦之行，西敏寺前的溫馨。
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+        {/* TODO: make PostCard compatible with links*/}
+        <Card sx={{}}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              image="https://ethanyangcx.github.io/love/pic/2022-0710-1-1.jpeg"
+              alt="photo of us taken at elevator"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                鷗崽電梯合照午灰
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {dayNumberFromLove('2022-07-10T17:03+01:00')} ❤️ 鷗崽（qou-sreej）（英：O'Shea）（愛：Ó Séaghdha）電梯見證了我們數不清的合照。
+                也是在這一天，我們在鷗崽負一樓鋼琴房即興合奏《神祕園之歌》並即興上傳視頻，見下方按鈕：
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary" href="https://v.douyin.com/23VEsxg/">
+              Video
+            </Button>
+          </CardActions>
+        </Card>
 
-          <Card sx={{}}> 
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image="https://ethanyangcx.github.io/love/pic/2022-0131-1-1.jpeg"
-                alt="happy new year"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  第一次相識
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  年夜飯上第一次相識，一起切土豆，一起包餃子，一起不上桌，一起回家。
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+        <PostCard 
+          image="https://ethanyangcx.github.io/love/pic/2022-0605-1-1.jpg"
+          alt="sweet hug in front of Westminster Abbey"
+          title="第一次旅遊"
+          main={dayNumberFromLove('2022-06-05T18:00+01:00') + 
+                " ❤️ 倫敦之行，西敏寺前的溫馨。"} 
+        />
+
+        <PostCard 
+          image="https://ethanyangcx.github.io/love/pic/2022-0131-1-1.jpeg"
+          alt="happy new year"
+          title="第一次相識"
+          main={dayNumberFromLove('2022-01-31T19:00+00:00') + 
+                " ❤️ 年夜飯上第一次相識，一起切土豆，一起包餃子，一起不上桌，一起回家。"}
+        />
 
       </Stack>
       </Container>
